c625d24415415c78ec6d37d39b24172c260d6070
mohamadarif03 <pecintarealmadrid2@gmail.com>
2025-08-08 18:56:59 +0700

    fix: title

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>{{ $title ?? 'Sistem Car Lease' }}</title>
+    <title>{{ $title ?? 'Dolfin Brain' }}</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="handheldfriendly" content="true" />
