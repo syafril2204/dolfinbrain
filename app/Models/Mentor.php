b0bacaf89bb0dcb52140db414f73bcd9373e6dde
mohamadarif03 <pecintarealmadrid2@gmail.com>
2025-08-17 08:33:13 +0700

    feat mentor

---
+++ app/Models/Mentor.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mentor extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'position_id',
+        'photo',
+        'description',
+    ];
+
+    public function position()
+    {
+        return $this->belongsTo(Position::class);
+    }
+}
