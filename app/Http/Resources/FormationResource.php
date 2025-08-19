4fc252629cc86cafd3271e8719ff94cfb0b5dfe8
mohamadarif03 <pecintarealmadrid2@gmail.com>
2025-08-19 19:56:30 +0700

    Update FormationResource.php

--- app/Http/Resources/FormationResource.php
+++ app/Http/Resources/FormationResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class FormationResource extends JsonResource
 {
@@ -18,6 +19,7 @@ class FormationResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'short_description' => $this->short_description,
+            'image' => $this->image ? Storage::disk('public')->url($this->image) : null,
         ];
     }
 }
