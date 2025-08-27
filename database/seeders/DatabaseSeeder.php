084692365007208e2e79ba69ce7cd992327ce271
BerlianPurliN <novtengpn14@gmail.com>
2025-08-27 13:06:15 +0700

    Menambahkan Seeder Mentor Dan Artikel'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
             FormationAndPositionSeeder::class,
             MaterialAndQuizSeeder::class,
             LmsSpaceSeeder::class,
+            MentorSeeder::class,
+            ArticleSeeder::class,
         ]);
     }
 }
